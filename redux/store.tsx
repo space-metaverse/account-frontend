f6a2c5f206780365ab9fd8e33ba6ff3bd1c968f3
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-10-26 12:23:41 -0600

    remove old auth API

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,15 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { authApi } from "../api/auth";
 import appReducer from "./slices/app";
 
 export const store = configureStore({
     reducer: {
         app: appReducer,
-        [authApi.reducerPath]: authApi.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
-        authApi.middleware,
+
     ),
 });
 
