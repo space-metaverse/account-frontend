8c68f5f7e0d0ebf8144942f99eb41f10fa7f4c3f
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-10-27 12:31:49 -0600

    upgrade space-ui ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'styled-components';
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
-import { Theme, GlobalStyles } from '@space-metaverse-ag/space-ui/styledTheme';
+import { ThemeProvider } from '@space-metaverse-ag/space-ui';
 
 function App({ Component, pageProps }: AppProps) {
   return <Provider store={store}>
-    <ThemeProvider theme={Theme}>
-      <GlobalStyles />
+    <ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
   </Provider>
