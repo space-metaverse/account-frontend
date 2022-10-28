04561a689ef47be079f686584cb39506417efad2
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-10-28 10:37:36 -0600

    change casing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux'
 
 import { ThemeProvider, GlobalStyles } from '@space-metaverse-ag/space-ui'
-import TopNav from 'layouts/TopNav'
+import TopNav from 'layouts/topnav'
 import type { AppProps } from 'next/app'
 import { store } from 'redux/store'
 import styled from 'styled-components'
