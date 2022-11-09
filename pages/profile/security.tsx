5fa0f99e170d7ded81d2702c0d07efd0c110e977
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-11-09 09:46:06 -0700

    linter fixes

--- pages/profile/security.tsx
+++ pages/profile/security.tsx
@@ -1,11 +1,11 @@
-import { type ReactElement } from "react";
+import { type ReactElement } from 'react'
 
-import { Button, TextInput } from "@space-metaverse-ag/space-ui";
-import Layout from "layouts/profile";
-import Head from "next/head";
-import styled from "styled-components";
+import { Button, TextInput } from '@space-metaverse-ag/space-ui'
+import Layout from 'layouts/profile'
+import Head from 'next/head'
+import styled from 'styled-components'
 
-import type { NextPageWithLayout } from "../../types";
+import type { NextPageWithLayout } from '../../types'
 
 const Form = styled.div`
   gap: 1rem;
@@ -19,7 +19,7 @@ const Form = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
   }
-`;
+`
 
 const Actions = styled.div`
   gap: 0.75rem;
@@ -27,14 +27,14 @@ const Actions = styled.div`
   display: flex;
   border-top: ${({ theme }) => `1px solid ${theme.colors.dark[200]}`};
   align-items: center;
-`;
+`
 
 const Container = styled.div`
   width: 100%;
   display: flex;
   max-width: 37.5rem;
   flex-direction: column;
-`;
+`
 
 const Label = styled.label`
   text-transform: uppercase;
@@ -45,7 +45,7 @@ const Label = styled.label`
   letter-spacing: 0.04em;
   text-transform: uppercase;
   color: #111114;
-`;
+`
 
 const Security: NextPageWithLayout = () => {
   return (
@@ -54,47 +54,47 @@ const Security: NextPageWithLayout = () => {
         <Label>Change Password</Label>
 
         <Form>
-          <div className="is-grid">
+          <div className='is-grid'>
             <TextInput
-              label="Current Password"
-              placeholder="Enter your current password"
-              type="password"
+              label='Current Password'
+              placeholder='Enter your current password'
+              type='password'
             />
           </div>
 
-          <div className="is-grid">
+          <div className='is-grid'>
             <TextInput
-              label="New Password"
-              placeholder="Enter new password"
-              type="password"
+              label='New Password'
+              placeholder='Enter new password'
+              type='password'
             />
 
             <TextInput
-              label="Confirm New Password"
-              placeholder="Re-enter new password"
-              type="password"
+              label='Confirm New Password'
+              placeholder='Re-enter new password'
+              type='password'
             />
           </div>
         </Form>
         <Actions>
-          <Button size="medium" color="blue" label="Update password" />
+          <Button size='medium' color='blue' label='Update password' />
 
-          <Button size="medium" color="white-red" label="Discard" />
+          <Button size='medium' color='white-red' label='Discard' />
         </Actions>
       </Container>
     </>
-  );
-};
+  )
+}
 
 Security.getLayout = (page: ReactElement) => (
-  <Layout title="Security Settings">
+  <Layout title='Security Settings'>
     <Head>
       <title>Security Settings | SPACE</title>
-      <meta name="description" content="SPACE Accounts" />
+      <meta name='description' content='SPACE Accounts' />
     </Head>
 
     {page}
   </Layout>
-);
+)
 
-export default Security;
+export default Security
